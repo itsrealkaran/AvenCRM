82ce08538058db72a9cc90253bb53da4982462f0
Abhinandan <abhinandanverma551@gmail.com>
2024-12-11 09:38:29 +0530

    tranaszctions update

--- frontend/app/(dashboard)/agent/transactions/page.tsx
+++ frontend/app/(dashboard)/agent/transactions/page.tsx
@@ -131,11 +131,44 @@ export default function TransactionsPage() {
 
   if (isLoading) {
     return (
-      <div className='flex flex-col space-y-3'>
-        <Skeleton className='w-[800px] h-[80px]' />
-        <div className='space-y-2'>
-          <Skeleton className='h-4 w-[250px]' />
-          <Skeleton className='h-4 w-[200px]' />
+      <div className='container mx-auto py-10'>
+        <div className='flex justify-between items-center p-5'>
+          <div className='space-y-3'>
+            <Skeleton className='h-8 w-64' />
+            <Skeleton className='h-4 w-48' />
+          </div>
+          <Skeleton className='h-10 w-40' />
+        </div>
+
+        <div className='space-y-4 p-6'>
+          <div className='flex justify-between items-center'>
+            <Skeleton className='h-10 w-64' />
+            <Skeleton className='h-10 w-32' />
+          </div>
+
+          <div className='rounded-md border'>
+            <div className='space-y-4'>
+              {[...Array(5)].map((_, idx) => (
+                <div key={idx} className='flex items-center justify-between p-4 border-b'>
+                  <div className='flex items-center space-x-4'>
+                    <Skeleton className='h-4 w-4' />
+                    <Skeleton className='h-4 w-32' />
+                    <Skeleton className='h-4 w-24' />
+                    <Skeleton className='h-4 w-20' />
+                    <Skeleton className='h-4 w-16' />
+                    <Skeleton className='h-8 w-40' />
+                    <Skeleton className='h-4 w-24' />
+                  </div>
+                  <Skeleton className='h-8 w-8' />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className='flex justify-end space-x-2 mt-4'>
+            <Skeleton className='h-8 w-24' />
+            <Skeleton className='h-8 w-24' />
+          </div>
         </div>
       </div>
     );
