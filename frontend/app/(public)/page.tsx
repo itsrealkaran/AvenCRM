68d7d7885e4b0aca91c05e7f68729e95294dcef6
Pushkar Kamble <pushkarkamble23@gmail.com>
2024-12-12 19:11:23 +0530

    minor fix

--- frontend/app/(public)/page.tsx
+++ frontend/app/(public)/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { useEffect } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { ArrowRight } from 'lucide-react';
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/sign-in');
+  });
   return (
     <div className='bg-gradient-to-b from-white to-gray-50'>
       <main className='min-h-screen'>
