0a9b8ecc2097e6a63a5e02204512d305e333f8fd
itsrealkaran <karansingh@duck.com>
2025-06-05 07:55:16 +0530

    feat: Page Builder

--- frontend/app/(dashboard)/agent/page-builder/page.tsx
+++ frontend/app/(dashboard)/agent/page-builder/page.tsx
@@ -1,176 +1,43 @@
 'use client';
 
-import React from 'react';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
-import { Button } from '@/components/ui/button';
+import Contact from '@/components/page-builder/contact-view';
+import Dashboard from '@/components/page-builder/dashboard-view';
+import DocumentDownload from '@/components/page-builder/document-download-view';
+import LocationSearch from '@/components/page-builder/location-search-view';
+import PortfolioPage from '@/components/page-builder/portfolio-view';
+import UpdateContact from '@/components/page-builder/update/contact/page';
+import UpdateDocumentDownload from '@/components/page-builder/update/document-download/page';
+import UpdateLocationSearch from '@/components/page-builder/update/location-search/page';
+import SetupForm from '@/components/page-builder/update/portfolio/page';
 
-function PageBuilder() {
-  const router = useRouter();
-  return (
-    <div className='flex-1 h-screen overflow-y-auto space-y-4 p-4 md:p-6 rounded-xl z-20 shadow-lg bg-white'>
-      <h3 className='text-2xl font-bold text-center'>Template</h3>
-      <div className='relative'>
-        <button
-          onClick={() => document.getElementById('scroll-container')?.scrollBy(-200, 0)}
-          className='absolute left-0 top-1/2 -translate-y-1/2 z-10 bg-white/80 p-2 rounded-full shadow-md hover:bg-white'
-        >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-6 h-6'
-          >
-            <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
-          </svg>
-        </button>
-        <div
-          id='scroll-container'
-          className='flex gap-4 overflow-x-auto scroll-smooth scrollbar-hide px-8'
-        >
-          <div
-            className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'
-            onClick={() => {
-              router.push('/agent/marketing/add');
-            }}
-          >
-            <Image
-              src={'/template1.png'}
-              alt='not showing '
-              width={100}
-              height={100}
-              className='w-52 h-52 border-2 border-black/60 rounded-md'
-            />
-          </div>
-          <div className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'>
-            <Image
-              src={'/template1.png'}
-              alt='not showing '
-              width={100}
-              height={100}
-              className='w-52 h-52 border-2 border-black/60 rounded-md'
-            />
-          </div>
-          <div className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'>
-            <Image
-              src={'/template1.png'}
-              alt='not showing '
-              width={100}
-              height={100}
-              className='w-52 h-52 border-2 border-black/60 rounded-md'
-            />
-          </div>
-          <div className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'>
-            <Image
-              src={'/template1.png'}
-              alt='not showing '
-              width={100}
-              height={100}
-              className='w-52 h-52 border-2 border-black/60 rounded-md'
-            />
-          </div>
-          <div className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'>
-            <Image
-              src={'/template1.png'}
-              alt='not showing '
-              width={100}
-              height={100}
-              className='w-52 h-52 border-2 border-black/60 rounded-md'
-            />
-          </div>
-        </div>
-        <button
-          onClick={() => document.getElementById('scroll-container')?.scrollBy(200, 0)}
-          className='absolute right-0 top-1/2 -translate-y-1/2 z-10 bg-white/80 p-2 rounded-full shadow-md hover:bg-white'
-        >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-6 h-6'
-          >
-            <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
-          </svg>
-        </button>
-      </div>
+export default function PageBuilder() {
+  const [currentView, setCurrentView] = useState('dashboard');
+  const [currentPageId, setCurrentPageId] = useState<string | undefined>(undefined);
 
-      {/* My Templates */}
+  const navigateTo = (view: string, pageId?: string) => {
+    console.log(`Navigating to ${view} with pageId:`, pageId);
+    setCurrentView(view);
+    setCurrentPageId(pageId);
+    window.scrollTo(0, 0);
+  };
 
-      <h2 className='text-2xl font-bold text-center'>My Templates</h2>
-      <div className='flex flex-wrap gap-4 px-8'>
-        <div className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'>
-          <Image
-            src={'/template1.png'}
-            alt='not showing '
-            width={100}
-            height={100}
-            className='w-52 h-52 border-2 border-black/60 rounded-md'
-          />
-          <div className='flex gap-2 mt-4'>
-            <Button>Share</Button>
-            <Button>Edit</Button>
-          </div>
-        </div>
-        <div className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'>
-          <Image
-            src={'/template1.png'}
-            alt='not showing '
-            width={100}
-            height={100}
-            className='w-52 h-52 border-2 border-black/60 rounded-md'
-          />
-          <div className='flex gap-2 mt-4'>
-            <Button>Share</Button>
-            <Button>Edit</Button>
-          </div>
-        </div>
-        <div className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'>
-          <Image
-            src={'/template1.png'}
-            alt='not showing '
-            width={100}
-            height={100}
-            className='w-52 h-52 border-2 border-black/60 rounded-md'
-          />
-          <div className='flex gap-2 mt-4'>
-            <Button>Share</Button>
-            <Button>Edit</Button>
-          </div>
-        </div>
-        <div className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'>
-          <Image
-            src={'/template1.png'}
-            alt='not showing '
-            width={100}
-            height={100}
-            className='w-52 h-52 border-2 border-black/60 rounded-md'
-          />
-          <div className='flex gap-2 mt-4'>
-            <Button>Share</Button>
-            <Button>Edit</Button>
-          </div>
-        </div>
-        <div className='w-fit h-fit p-4 bg-zinc-200 rounded-md flex-shrink-0'>
-          <Image
-            src={'/template1.png'}
-            alt='not showing '
-            width={100}
-            height={100}
-            className='w-52 h-52 border-black/60 rounded-md'
-          />
-          <div className='flex gap-2 mt-4'>
-            <Button>Share</Button>
-            <Button>Edit</Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  const renderView = () => {
+    switch (currentView) {
+      case 'portfolio':
+        return <PortfolioPage navigateTo={navigateTo} pageId={currentPageId} />;
+      case 'location-search':
+        return <LocationSearch navigateTo={navigateTo} pageId={currentPageId} />;
+      case 'document-download':
+        return <DocumentDownload navigateTo={navigateTo} pageId={currentPageId} />;
+      case 'contact':
+        return <Contact navigateTo={navigateTo} pageId={currentPageId} />;
+      case 'dashboard':
+      default:
+        return <Dashboard navigateTo={navigateTo} />;
+    }
+  };
+
+  return <>{renderView()}</>;
 }
-
-export default PageBuilder;
